f88e5fc8408ce8405fdfe08a1365dc81c34c8634
zeelpatel1 <zeelpatel6878@gmail.com>
2026-03-01 10:56:16 +0530

    we have setuped routes for /tasks initially

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { Hono } from 'hono'
+import { z } from 'zod'
+import tasksRouter from './routes/tasks'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.route('/tasks',tasksRouter)
+
+app.notFound((c) => {
+  return c.json({ error: 'Not Found' }, 404)
+})
+
+export default app
---
+++ src/routes/tasks.ts
@@ -0,0 +1,55 @@
+import z from "zod";
+import { Hono } from "hono";
+
+const tasksRouter=new Hono()
+
+const taskSchema=z.object({
+    title:z.string().min(1)
+  })
+  
+type Task= {
+    id:number,
+    title:string
+}
+
+type Tasks=Task[];
+
+const tasks:Tasks=[];
+
+tasksRouter.get('/',(c)=>{
+    return c.json(tasks)
+})
+
+tasksRouter.post('/', async (c) => {
+    const { title } = await c.req.json();
+  
+    const validateData = taskSchema.safeParse({ title });
+  
+    if(!validateData.success){
+      return c.json({error:validateData.error.message},400)
+    }
+    const newTask:Task={
+      id:tasks.length+1,
+      title
+    }
+  
+    tasks.push(newTask) 
+  
+    return c.json(newTask)
+  
+  })
+  
+  tasksRouter.delete('/:id',(c)=>{
+    const id=Number(c.req.param('id'))
+  
+    const index=tasks.findIndex(task=>task.id===id)
+    if(index===-1){
+      return c.json({error:'Task not found'},404)
+    }
+  
+    tasks.splice(index,1)
+    return c.json({message:'Task deleted successfully'},200)
+  
+})
+
+export default tasksRouter;
